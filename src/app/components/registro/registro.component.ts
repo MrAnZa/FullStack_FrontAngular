54cd2ade31be596c92dc8897edb28ab29d90ebda
Andrés Zamora <andreszamora1998@gmail.com>
2020-02-15 23:43:37 -0500

    Formulario y Servicio de Usuarios Agregados

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
+  providers: [UserService]
 })
 export class RegistroComponent implements OnInit {
   public page_title: string;
-  constructor() { 
+  public user: User;
+  constructor(private _userService: UserService) { 
     this.page_title='Registrate';
-  }
+    this.user= new User(1,'','','ROLE_USER','','','','');
+ }
 
   ngOnInit() {
+  }
+  onSubmit(form){
+    console.log(this.user);
+    console.log(this._userService.test());
+  form.reset();
   }
 
 }
