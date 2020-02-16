4cbf90fb9a12a68c7d9a96fe580d47e8adcc6c6a
Andrés Zamora <andreszamora1998@gmail.com>
2020-02-16 15:06:45 -0500

    Peticion POST para Guardar Usuario

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../models/user';
-import {UserService} from '../../services/user.service';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-registro',
@@ -11,17 +11,30 @@ import {UserService} from '../../services/user.service';
 export class RegistroComponent implements OnInit {
   public page_title: string;
   public user: User;
-  constructor(private _userService: UserService) { 
-    this.page_title='Registrate';
-    this.user= new User(1,'','','ROLE_USER','','','','');
- }
+  public status: string;
+
+  constructor(private _userService: UserService) {
+    this.page_title = 'Registrate';
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
+  }
 
   ngOnInit() {
   }
-  onSubmit(form){
-    console.log(this.user);
-    console.log(this._userService.test());
-  form.reset();
+  onSubmit(form) {
+    this._userService.register(this.user).subscribe(
+      response => {
+        if (response.status == "success") {
+          this.status = response.status;
+          form.reset();
+        } else {
+          this.status = "Error";
+        }
+      },
+      error => {
+        this.status = "Error";
+        console.log(<any>error);
+      }
+    );
   }
 
 }
