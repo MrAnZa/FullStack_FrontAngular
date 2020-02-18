87443b3c11ab75dd7187bc685644a12609e17a41
Andrés Zamora <andreszamora1998@gmail.com>
2020-02-17 22:43:36 -0500

    Identificar Usuario (Login)Angular

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../models/user';
-import {UserService} from '../../services/user.service';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +9,14 @@ import {UserService} from '../../services/user.service';
   providers: [UserService]
 })
 export class LoginComponent implements OnInit {
-public page_title: string;
-public user:User
+  public page_title: string;
+  public user: User
+  public status: string;
+  public token;
+  public identity;
   constructor(
-    private _userService:UserService
-  ) { 
+    private _userService: UserService
+  ) {
     this.page_title = 'Identificate';
     this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
   }
@@ -21,8 +24,35 @@ public user:User
   ngOnInit() {
   }
 
-  onSubmit(form){
-console.log(this.user);
+  onSubmit(form) {
+    this._userService.signup(this.user).subscribe(
+      response => {
+        //TOKEN
+        if (response.status != 'error') {
+          this.status = 'success';
+          this.token = response;
+          //Objeto Identificado
+          this._userService.signup(this.user,true).subscribe(
+            response => {
+                this.identity = response;
+                
+            console.log(this.token);
+            console.log(this.identity);
+            },
+            error => {
+              this.status = 'error';
+              console.log(<any>error);
+            }
+          )
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    )
   }
 
 }
