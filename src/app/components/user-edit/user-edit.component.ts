b61305a5d2b5e87448691e37da3bf94a17522bed
Andrés Zamora <andreszamora1998@gmail.com>
2020-03-06 20:38:01 -0500

    Guardar Cambios de Usuario

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -13,6 +13,7 @@ export class UserEditComponent implements OnInit {
   public user: User;
   public identity;
   public token;
+  public status:string;
 
   constructor(
     private _userService: UserService
@@ -22,9 +23,54 @@ export class UserEditComponent implements OnInit {
     this.identity=this._userService.getIdentity();
     this.token=this._userService.getToken();
     this.user=this.identity;
+    this.user= new User(
+      this.identity.sub,
+      this.identity.name,
+      this.identity.surname,
+      this.identity.role,
+      this.identity.email,'',
+      this.identity.description,
+      this.identity.image);
   }
 
   ngOnInit() {
+  }
+  onSubmit(form){
+    this._userService.update(this.token,this.user).subscribe(
+      response => {
+        console.log(response);
+        if(response){
+          this.status='success';
+          if(response.changes.name){
+            this.user.name=response.changes.name;
+          }
+          if(response.changes.surname){
+            this.user.surname=response.changes.surname;
+          }
+          if(response.changes.email){
+            this.user.email=response.changes.email;
+          }
+          if(response.changes.description){
+            this.user.description=response.changes.description;
+          }
+          if(response.changes.image){
+            this.user.image=response.changes.image;
+          }
+          //Sesion
+          this.identity=response.user;
+          //Local
+          localStorage.setItem('identity',JSON.stringify(this.identity));
+        }else{
+          this.status='error';
+        }
+
+      },
+      error => {
+        this.status='error';
+        console.log(<any>error);
+      }
+    )
+
   }
 
 }
