1af6de34ac2c40e7a46f6432b99f46b45afdf63e
Andres Zamora <andreszamora1998@gmail.com>
2020-04-26 20:50:38 -0500

    Obtener Entradas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostService } from '../../services/post.service';
+import { global } from 'src/app/services/global';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[PostService]
 })
 export class HomeComponent implements OnInit {
 public page_title: string;
-  constructor() { 
+public url;
+public posts:Array<Post>;
+  constructor(private _postService:PostService) { 
     this.page_title='Inicio';
+    this.url=global.url;
   }
 
   ngOnInit() {
+    this.getPosts();
   }
-
+getPosts(){
+  this._postService.getPosts().subscribe(
+    response =>{
+      if(response.status='success'){
+        this.posts=response.post;
+        console.log(this.posts);
+      }
+    },error=>{
+      console.log(error);
+    }
+  )
+}
 }
