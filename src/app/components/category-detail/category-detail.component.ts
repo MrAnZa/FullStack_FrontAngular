a8304dc1ce2c5a89e7e2e5328f9a6221da45d809
Andres Zamora <andreszamora1998@gmail.com>
2020-04-30 21:55:09 -0500

    detalle de las categorias

--- src/app/components/category-detail/category-detail.component.ts
+++ src/app/components/category-detail/category-detail.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute,Params } from '@angular/router';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
+import { global } from '../../services/global';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-category-detail',
   templateUrl: './category-detail.component.html',
-  styleUrls: ['./category-detail.component.css']
+  styleUrls: ['./category-detail.component.css'],
+  providers: [CategoryService]
 })
 export class CategoryDetailComponent implements OnInit {
+ public page_title:string;
+ public category:Category;
+ public posts: Post[];
+ public url: string;
 
-  constructor() { }
+  constructor(
+    private _route:ActivatedRoute,
+    private _router:Router,
+    private _categoryService:CategoryService
+  ) { 
+    this.url=global.url;
+  }
 
   ngOnInit() {
+this.getPostsByCategory();
+  }
+
+  getPostsByCategory(){
+    this._route.params.subscribe(params=>{
+      let id=params['id'];
+      this._categoryService.getCategory(id).subscribe(response=>{
+        if(response.status='success'){
+          this.category=response.category;
+          this.page_title=this.category.name;
+        }else{
+          this._router.navigate(['/inicio']);
+        }
+      },error=>{
+        console.log(error);
+      })
+    })
   }
 
 }
